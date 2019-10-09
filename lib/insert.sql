9ed4379dd89cb261b93cc12994b8cfd1f33d499e
Dean L Betty <deanlloyd64@gmail.com>
2019-10-09 01:06:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'Female', 'Nervous', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'Male', 'Grumpy', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 14, 'Female', 'Calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( NULL, 14, 'Female', 'Rowdy', 1);
